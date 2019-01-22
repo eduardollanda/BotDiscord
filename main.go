29b8554d0f216a629cbafc96f039a84bcff630e3
eduardollanda <35275805+eduardollanda@users.noreply.github.com>
2019-01-22 18:44:46 -0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// token de acesso
+const token string = "NTE0ODM5MDU2Mzc1MzQ5MzIx.DyJoqA.j0uvCQzt8zAXyVamvDgsVRlup5Y"
+
+// Variavel global para poder ser lida por outros pacotes
+var BotID string
+
+func main() {
+
+	discord, err := discordgo.New("Bot " + token)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	botUser, err := discord.User("@me")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// Associando o ID do bot a variavel global criada antes
+	BotID = botUser.ID
+
+	discord.AddHandler(messageHandler)
+
+	err = discord.Open()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("O bot esta online!")
+
+	// Canal vazio para garantir que o bot estará sempre rodando e esperando algum evento
+	<-make(chan struct{})
+
+}
+func messageHandler(session *discordgo.Session, message *discordgo.MessageCreate) {
+
+	if message.Author.ID == BotID {
+		return
+	}
+
+	if message.Content == "ping" {
+		session.ChannelMessageSend(message.ChannelID, "pong")
+	}
+}
